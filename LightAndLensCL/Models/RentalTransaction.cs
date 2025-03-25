9ad4a3056c50777434c2c4bed3473c6a010990e3
Talal <202201761@student.polytechnic.bh>
2025-03-25 04:51:48 +0300

    Class Library and Database .bak file added

---
+++ LightAndLensCL/Models/RentalTransaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LightAndLensCL.Models
+{
+    public partial class RentalTransaction
+    {
+        public RentalTransaction()
+        {
+            RentalItems = new HashSet<RentalItem>();
+            ReturnRecords = new HashSet<ReturnRecord>();
+        }
+
+        [Key]
+        [Column("RentalID")]
+        public int RentalId { get; set; }
+        [Column("UserID")]
+        public int UserId { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime StartDate { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime EndDate { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal RentalFee { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal? DepositPaid { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Status { get; set; } = null!;
+
+        [ForeignKey("UserId")]
+        [InverseProperty("RentalTransactions")]
+        public virtual User User { get; set; } = null!;
+        [InverseProperty("Rental")]
+        public virtual ICollection<RentalItem> RentalItems { get; set; }
+        [InverseProperty("Rental")]
+        public virtual ICollection<ReturnRecord> ReturnRecords { get; set; }
+    }
+}
