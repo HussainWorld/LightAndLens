27852efdf21f22deaf8d90471c473254d99f2fce
Ali Ebrahim Mohamed Ameen Ebrahim Alarab <202200070@student.polytechnic.bh>
2025-03-25 06:52:43 +0300

    Restore Database and Scaffold Models
    Restored the Northwind database from a .bak file using SQL Server Management Studio (SSMS).
    
    Scaffolded the models and DbContext for the database in the Class Library using Entity Framework.
    
    The database is now ready for further development using the Database-First approach.

--- LightAndLensCL/obj/Debug/net6.0/LightAndLensCL.AssemblyInfo.cs
+++ LightAndLensCL/obj/Debug/net6.0/LightAndLensCL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightAndLensCL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad4a3056c50777434c2c4bed3473c6a010990e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightAndLensCL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightAndLensCL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
