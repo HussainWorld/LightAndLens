e6cb6b5b95962d5d8ebf04b937d2c7b10ff36a9c
Talal <202201761@student.polytechnic.bh>
2025-04-16 21:38:47 +0300

    Added full DB class library alongside .bak file
    Identity will be added after web app development starts, will work retroactively after.

---
+++ LightAndLensCL/LightAndLensCL/Models/Equipment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LightAndLensCL.Models
+{
+    public partial class Equipment
+    {
+        public Equipment()
+        {
+            EquipmentImages = new HashSet<EquipmentImage>();
+            Feedbacks = new HashSet<Feedback>();
+            RentalItems = new HashSet<RentalItem>();
+            RentalRequests = new HashSet<RentalRequest>();
+        }
+
+        [Key]
+        [Column("EquipmentID")]
+        public int EquipmentId { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string EquipmentName { get; set; } = null!;
+        [Column(TypeName = "text")]
+        public string Description { get; set; } = null!;
+        [Column("CategoryID")]
+        public int CategoryId { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal RentalPricePerDay { get; set; }
+        public int Quantity { get; set; }
+        [Column("ConditionID")]
+        public int ConditionId { get; set; }
+        [Column("AvailabilityID")]
+        public int? AvailabilityId { get; set; }
+
+        [ForeignKey("AvailabilityId")]
+        [InverseProperty("Equipment")]
+        public virtual AvailabilityStatus? Availability { get; set; }
+        [ForeignKey("CategoryId")]
+        [InverseProperty("Equipment")]
+        public virtual Category Category { get; set; } = null!;
+        [ForeignKey("ConditionId")]
+        [InverseProperty("Equipment")]
+        public virtual ConditionStatus Condition { get; set; } = null!;
+        [InverseProperty("Equipment")]
+        public virtual ICollection<EquipmentImage> EquipmentImages { get; set; }
+        [InverseProperty("Equipment")]
+        public virtual ICollection<Feedback> Feedbacks { get; set; }
+        [InverseProperty("Equipment")]
+        public virtual ICollection<RentalItem> RentalItems { get; set; }
+        [InverseProperty("Equipment")]
+        public virtual ICollection<RentalRequest> RentalRequests { get; set; }
+    }
+}
---
+++ LightAndLensCL/LightAndLensCL/Models/UserDocument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace LightAndLensCL.Models
+{
+    public partial class UserDocument
+    {
+        [Key]
+        [Column("DocumentID")]
+        public int DocumentId { get; set; }
+        [Column("UserID")]
+        [StringLength(450)]
+        public string UserId { get; set; } = null!;
+        [StringLength(250)]
+        public string FileName { get; set; } = null!;
+        [StringLength(50)]
+        public string FileType { get; set; } = null!;
+        public byte[] FileData { get; set; } = null!;
+    }
+}
